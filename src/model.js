1d85af38f47e40b3d07c6166de358fb8f7af20c3
jiangjj <jiangjj@sse.net.cn>
2022-06-30 12:16:11 +0800

    first commit

---
+++ src/model.js
@@ -0,0 +1,24 @@
+const model = [
+    {
+        name: `Amy的城堡纸盒`,
+        image: `https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg9.51tietu.net%2Fpic%2F2019-091203%2Fgjafyylfihngjafyylfihn.jpg&refer=http%3A%2F%2Fimg9.51tietu.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1659062670&t=d3ab1d6f979468badbcdd7214e7795f5`,
+        body: '全新, 隐蔽性好',
+    },
+    {
+        name: 'Baddy的饼干盒',
+        image: `https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpicnew10.photophoto.cn%2F20151225%2Fkeaidexiaomao-28881076_1.jpg&refer=http%3A%2F%2Fpicnew10.photophoto.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1659062672&t=2b7948141985923990b9d7673ca9f368`,
+        body: '适合瘦身塑行',
+    },
+    {
+        name: 'Corney的飞行纸盒',
+        image: `https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg7.51tietu.net%2Fpic%2F2019-082117%2Fgxuutpky2f1gxuutpky2f1.jpg&refer=http%3A%2F%2Fimg7.51tietu.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1659062675&t=33c0d832a2e8ff8eeb33b5c7d1d80653`,
+        body: '时尚折边',
+    },
+    {
+        name: 'Lanzi的敞篷纸盒',
+        image: `https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg9.51tietu.net%2Fpic%2F2019-091305%2Fu35w052dxlau35w052dxla.jpg&refer=http%3A%2F%2Fimg9.51tietu.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1659062686&t=80abbb8f763f7b57b4f19a4b1181a9f6`,
+        body: '超大空间，买就送鱼罐头',
+    }
+];
+
+export default model;
